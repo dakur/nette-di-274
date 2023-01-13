a7057cdb94b50af6580400a653ed28804dca919b
Daniel Kurowski <daniel.kurowski@grifart.cz>
2023-01-13 11:24:14 +0100

    implement model in presenter

--- app/Presenters/HomepagePresenter.php
+++ app/Presenters/HomepagePresenter.php
@@ -4,9 +4,30 @@ declare(strict_types=1);
 
 namespace App\Presenters;
 
+use App\Model\FileFacade;
 use Nette;
+use Nette\Application\UI\Form;
 
 
 final class HomepagePresenter extends Nette\Application\UI\Presenter
 {
+
+	public function __construct(
+		private FileFacade $fileFacade,
+	) {
+		parent::__construct();
+	}
+
+	public function createComponentSaveFile(): Form
+	{
+		$form = new Form();
+		$form->addSubmit('submit', 'Save file');
+		$form->onSuccess[] = function () {
+			$this->fileFacade->save();
+			$this->flashMessage('File successfully stored');
+			$this->redirect('this');
+		};
+		return $form;
+	}
+
 }
